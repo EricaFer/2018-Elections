164322823521daf7ce6571c3079fcdf3d174d374
EricaFer <erica.ferreira@poli.ufrj.br>
2021-02-17 20:40:23 -0300

    Preprocessing Done. All symbols removed. Hashtag and Mentions col created

--- Preprocessing.py
+++ Preprocessing.py
@@ -2,25 +2,24 @@
 # Contact: erica.ferreira@poli.ufrj.br
 # Data source: https://github.com/RodrigoMenegat/o-que-15-mil-tweets-revelam-sobre-seu-candidato
 
-import pandas as pd
-from nltk.tokenize import TweetTokenizer
-from nltk.corpus import stopwords
-from spacy.lang.pt.stop_words import STOP_WORDS
+import re
 import nltk
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+from spacy.lang.pt.stop_words import STOP_WORDS
+
 nltk.download('stopwords')
 
 PATH_list =        ["alvarodias_.csv",
                     "cirogomes.csv",
-                    "collor.csv",
                     "geraldoalckmin.csv",
-                    "GuilhermeBoulos.csv",
                     "Haddad_Fernando.csv",
                     "jairbolsonaro.csv",
                     "joaoamoedonovo.csv",
                     "lulapelobrasil.csv",
-                    "meirelles.csv",
                     "MichelTemer.csv",
-                    "RodrigoMaia.csv",
+                    "meirelles.csv",
                     "silva_marina.csv"]
 
 candidate_list =   ['AlvaroDias',
@@ -62,11 +61,32 @@ def preprocessing(dfs,candidate_list):
     
     for key in dfs:
         
+        # Tokenizing - takes a phrase and isolate each word
         dfs[key]['token_list'] = dfs[key].apply(lambda x: tt.tokenize(x.text), axis = 1)
+        
+        # Dropping unnecessary labels
         dfs[key].drop(labels = ['id', 'datetime','text'], axis = 1, inplace = True)
+        
+        # Lowering words
         dfs[key]['token_list'] = [[word.lower() for word in lists] for lists in dfs[key].token_list]
+        
+        # Removing Stop Words - connectives, prepositions...
         dfs[key]['token_list'] = [[word for word in lists if word not in All_Stop_Words]for lists in dfs[key].token_list]
         
+        # Separating Hashtags
+        dfs[key]['Hashtag'] = [[word[1:] for word in lists if re.match('#', word) is not None] for lists in dfs[key].token_list]
+        
+        # Separating Twitter Mentions
+        dfs[key]['Mentions'] = [[word[1:] for word in lists if re.match('@', word) is not None] for lists in dfs[key].token_list]
+        
+        # Removing Links, Hashtags and Mentions
+        pattern_twitter = '((https)|@|#)'
+        dfs[key]['token_list'] = [[word for word in lists if re.match(pattern_twitter, word) is None ] for lists in dfs[key].token_list]
+        
+        # Removing all symbols
+        pattern_words_numbers = '[0-9a-zA-Z]+'
+        dfs[key]['token_list'] = [[word for word in lists if re.match(pattern_words_numbers, word) is not None] for lists in dfs[key].token_list]
+
 def saving_changes(dfs):
     print('Saving Changes...')
     for key in dfs:
